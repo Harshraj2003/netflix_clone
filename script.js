36ccc7d5633a0bfc2a43abd87a853e3d657ab42a
Harshraj2003 <harshrajraj2003@gmail.com>
2024-11-08 00:56:37 +0530

    first commit for netflix_clone and test for git

---
+++ script.js
@@ -0,0 +1,288 @@
+let movies = [
+     {
+        name: "Avengers Endgame",
+        poster: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/be030e77758917.5c911d5121625.png",
+        Rating: 8.7
+
+     },
+
+     {
+        name: "Avtar",
+        poster: "https://lh6.googleusercontent.com/proxy/gfVpdQsBP6vsvmRJSZn6e8AsuoWAxUvsBUoZUaaQpsTbKXeeBuzTudUbazSR5jQdzYbURejLHFvEzp7UGDjLDiIZnHiwoilXwQ",
+        Rating : 8.9
+     },
+
+     {
+        name:"Spider Man",
+        poster : "https://mir-s3-cdn-cf.behance.net/project_modules/hd/84f0ea133455823.65f1b5d357550.png",
+        Rating : 8.6
+     },
+
+     {
+        name :"Oppenhimer",
+        poster : "https://preview.redd.it/hi-res-oppenheimer-poster-3158-x-5000-pixels-v0-vz4foin472ya1.jpg?width=640&crop=smart&auto=webp&s=5734f811cbb596946ad4f583b50a090e4b6eb355",
+        Rating : 9.1
+     },
+
+     {
+        name:"Lagaan",
+        poster: "https://www.tallengestore.com/cdn/shop/products/7381745860788A_c2344d65-35ea-48ec-8a65-3c94a780d5e6_grande.jpg?v=1683929451",
+        Rating: 9.2
+
+     },
+
+     {
+        name: "Tumbaad",
+        poster: "https://lh6.googleusercontent.com/proxy/b-Dj8RlGblXtO43JnUmixYgyuJS427Wg-eKga1mvZp5_zrXPUB4xPjwG5g6OASSk45PF5LSgsi7iDSa3JEfrEFrQ9S7iFwvbb0kpzfxjBf2WOya6",
+        Rating: 9.2
+     },
+
+     {
+        name: "Justice League",
+        poster:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/65425a1c-1d61-429b-bfa0-7dc046b30c9e/dab9vpk-3b49db7d-f0ac-4324-842e-92cc04c63db1.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY1NDI1YTFjLTFkNjEtNDI5Yi1iZmEwLTdkYzA0NmIzMGM5ZVwvZGFiOXZway0zYjQ5ZGI3ZC1mMGFjLTQzMjQtODQyZS05MmNjMDRjNjNkYjEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.0nORQh3pEesf9RulHRoDj1t1LLmdzypCQDOEpigoQHY",
+        Rating:9.1
+     },
+
+     {
+       name: "Iron Man",
+       poster: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4114fc16-d47f-4c44-a197-86b204c21239/dexjoon-16781b30-9ce0-4a9c-b852-0e7db8669e6b.png/v1/fill/w_1280,h_1897,q_80,strp/1__iron_man__2008__official_poster_by_sdstark_dexjoon-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTg5NyIsInBhdGgiOiJcL2ZcLzQxMTRmYzE2LWQ0N2YtNGM0NC1hMTk3LTg2YjIwNGMyMTIzOVwvZGV4am9vbi0xNjc4MWIzMC05Y2UwLTRhOWMtYjg1Mi0wZTdkYjg2NjllNmIucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.sWGzrs-2gUwNTxNb7-zOV0GdI5QSSj5q84yk5oC6LeY",
+       Rating: 8.5
+     },
+
+     {
+      name: "Batman Begins",
+      poster: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/e33b5481719625.5d0842679b6c7.jpg",
+      Rating: 8.5
+     },
+
+     {
+      name:"Superman Returns",
+      poster:"https://rukminim2.flixcart.com/image/850/1000/kz4gh3k0/poster/q/b/r/medium-superman-dc-comics-movies-henry-cavill-movie-matte-finish-original-imagb786pu8nvv2g.jpeg?q=90&crop=false",
+      Rating: 8.4
+     },
+
+     {
+      name:"Batman 2",
+      poster:"https://preview.redd.it/the-batman-ii-movie-poster-concept-made-by-me-2592-x-3840-v0-8fm4i3880f1b1.png?width=1080&crop=smart&auto=webp&s=fff351dd10e619c48f7f496dbf95e0e4af1ea91f",
+      Rating: 8.3
+     },
+
+     {
+      name: "Kick",
+      poster:"https://stat5.bollywoodhungama.in/wp-content/uploads/2016/03/83723409-306x393.jpg",
+      Rating :7.4
+     },
+
+     {
+      name:"Joker",
+      poster:"https://images.javatpoint.com/best/images/best-english-movies11.png",
+      Rating: 8.8
+     },
+
+     {
+      name:"Dangal",
+      poster:"https://m.media-amazon.com/images/M/MV5BMTQ4MzQzMzM2Nl5BMl5BanBnXkFtZTgwMTQ1NzU3MDI@._V1_.jpg",
+      Rating:9.2
+     },
+
+     {
+      name:"RRR",
+      poster:"https://stat4.bollywoodhungama.in/wp-content/uploads/2019/03/RRR-2022-306x393.jpeg",
+      Rating:9.1
+     },
+
+     {
+      name:"Troy",
+      poster:"https://i0.wp.com/images.posterjunction.com/Troy-poster-1020280957.jpg",
+      Rating: 7.4
+     },
+
+     {
+      name: "Morbius",
+      poster: "https://www.tvsbook.com/cdn-cgi/image/quality=85,format=auto/attachments/morbius-jpeg.11423/",
+      Rating:7.2
+     },
+
+     {
+      name: "Barbie",
+      poster:"https://upload.wikimedia.org/wikipedia/en/0/0b/Barbie_2023_poster.jpg",
+      Rating:8.9
+     },
+
+     {
+      name:"Top Gun: Maverick",
+      poster:"https://upload.wikimedia.org/wikipedia/en/1/13/Top_Gun_Maverick_Poster.jpg",
+      Rating:8.6
+     },
+
+     {
+      name:"Black Panther: Wakanda Forever",
+      poster: "https://upload.wikimedia.org/wikipedia/en/3/3b/Black_Panther_Wakanda_Forever_poster.jpg",
+      Rating:8.4
+     },
+
+     {
+      name:"Dune",
+      poster:"https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
+      Rating:8.6
+     },
+
+     {
+      name:"Tenet",
+      poster:"https://upload.wikimedia.org/wikipedia/en/1/14/Tenet_movie_poster.jpg",
+      Rating:8.3
+     },
+
+     {
+      name:"Ford v Ferrari",
+      poster:"https://upload.wikimedia.org/wikipedia/en/a/a4/Ford_v._Ferrari_%282019_film_poster%29.png",
+      Rating:9.1
+     },
+
+     {
+      name:"Black Panther",
+      poster:"https://upload.wikimedia.org/wikipedia/en/d/d6/Black_Panther_%28film%29_poster.jpg",
+      Rating:9.2
+     },
+
+     {
+      name:"First Man",
+      poster:"https://upload.wikimedia.org/wikipedia/en/a/a8/First_Man_%28film%29.png",
+      Rating:9.3
+     },
+
+     {
+      name:"Darkest Hour",
+      poster:"https://upload.wikimedia.org/wikipedia/en/3/38/Darkest_Hour_poster.png",
+      Rating:8.9
+     },
+
+     {
+      name:"Moonlight",
+      poster:"https://upload.wikimedia.org/wikipedia/en/8/84/Moonlight_%282016_film%29.png",
+      Rating:8.5
+     },
+
+     {
+      name:"La La Land",
+      poster:"https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png",
+      Rating:8.6
+     },
+
+     {
+      name:"Suicide Squad",
+      poster:"https://upload.wikimedia.org/wikipedia/en/5/5c/Suicide_Squad_%282016_film%29_poster.png",
+      Rating:8.5
+     },
+
+     {
+      name:"Zero Dark Thirty",
+      poster:"https://upload.wikimedia.org/wikipedia/en/7/77/ZeroDarkThirty2012Poster.jpg",
+      Rating:8.1
+     },
+
+     {
+      name:"The Fighter",
+      poster:"https://upload.wikimedia.org/wikipedia/en/9/93/The_Fighter_Poster.jpg",
+      Rating:8.2
+     },
+
+     {
+      name:"The Wolfman",
+      poster:"https://upload.wikimedia.org/wikipedia/en/1/14/Wolfman-final-small.jpg",
+      Rating:8.6
+     },
+
+     {
+      name:"The Secret in Their Eyes",
+      poster:"https://upload.wikimedia.org/wikipedia/en/9/91/Cartel-nuevo-de-el-secreto-de-sus-ojos.jpg",
+      Rating:8.1
+     },
+
+     {
+      name:"The Whale",
+      poster:"https://upload.wikimedia.org/wikipedia/en/f/f3/TheWhalePoster.jpg",
+      Rating:8.6
+     },
+
+     {
+      name:"Men in Black",
+      poster:"https://upload.wikimedia.org/wikipedia/en/f/fb/Men_in_Black_Poster.jpg",
+      Rating:8.5
+     }
+
+];
+
+
+
+
+
+function searchMovie()
+{
+    let movieName = document.getElementById('search').value;
+
+    if(movieName!=="")
+    {
+       let result = movies.filter(function(movie)
+       {
+            return movie.name.toUpperCase().includes(movieName.toUpperCase())
+       })
+     
+       
+       displayMovies(result);
+    }
+    else{
+      displayMovies(movies);
+    }
+    
+}
+
+function displayMovies(data)
+{
+   
+   document.getElementById("movies").innerHtML="";
+
+   let htmlString = ``;
+
+   for(let i=0; i < data.length; i++)
+   {
+          htmlString=htmlString +`
+              <div class="movie">
+                  <div class="overlay">
+
+                      <div class="video" > </div>
+
+                      <div class="details">
+
+                         <h2> ${data[i].name}</h2>
+                         <h3> IMDB : ${data[i].Rating}</h3>
+                        
+
+                      </div>
+
+                   </div>
+                    <img class="poster" src="${data[i].poster}" alt="${movies[i].name} poster">
+
+               </div>
+      `
+   }
+   console.log(htmlString);
+
+   document.getElementById("movies").innerHTML=htmlString;
+}
+
+displayMovies(movies);
+
+
+
+
+
+// let movie = document.createElement("div");
+// movie.classList.add("movies");
+
+// let overlay = document.createElement("div");
+// overlay.classList.add("overlay");
+
+// movie.appendChild(overlay);
+
+// console.log(movie);
